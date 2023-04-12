3eb83df8c1104587ed1a73eee822b1ff59da1f9d
Joshua Clarke Cushing <joshuacushing@new-host-10.home>
2023-04-12 10:55:35 -0400

    test state works

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      testState: "testing",
+    };
+  },
+  methods: {
+    test() {
+      console.log(this.testState);
+    },
+  },
+};
+</script>
 
-<template></template>
+<template>
+  <h1>Vue To-Do List</h1>
+  <button @click="test">test</button>
+</template>
 
 <style></style>
