c913ee84368d3fa3ccf65fc9ce6da9f52f8856f3
Joshua Clarke Cushing <joshuacushing@new-host-10.home>
2023-04-12 11:08:29 -0400

    test form works

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,24 @@ let id = 0;
 export default {
   data() {
     return {
+      newTodo: "test",
       todos: [{ id: id++, text: "test text" }],
       testDynamicText: "Vue To-Do List",
     };
   },
-  methods: {},
+  methods: {
+    addTodo() {
+      this.todos.push({ id: id++, text: this.newTodo });
+    },
+  },
 };
 </script>
 
 <template>
   <h1>{{ testDynamicText }}</h1>
+  <form @submit.prevent="addTodo">
+    <button>Add To-Do</button>
+  </form>
   <ul>
     <li v-for="todo in todos" :key="todo.id">
       {{ todo.text }}
